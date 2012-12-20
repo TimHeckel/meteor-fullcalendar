fc6d6b9dbd85cbfe50f5219b7908946d69e9e4d8
TimHeckel <tim@slatebox.com>
2012-12-20 03:10:07 -0600

    Initial Load

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+    summary: "Full-sized drag and drop event calendar (jQuery plugin)"
+});
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.add_files([
+        'css/fullcalendar.css',
+        'css/fullcalendar.print.css',
+        'lib/fullcalendar.js',
+        'lib/gcal.js'
+    ], 'client');
+});
